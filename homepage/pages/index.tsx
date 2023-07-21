dbb2bd5172a80b86fb6d45593fa314c91d2e05a3
ymjlabMax <sud665@ymjlab.com>
2023-07-21 11:32:51 +0900

    타입 추가

--- homepage/pages/index.tsx
+++ homepage/pages/index.tsx
@@ -8,14 +8,15 @@ import { User } from "@/types";
 export default function Home() {
     const user: User = {
         id: 1,
-        name: "John Doe",
-        email: "john@example.com",
+        name: "Max",
+        email: "sud665@naver.com",
         isAdmin: false,
     };
 
     return (
         <main className="flex min-h-screen flex-col items-center justify-between p-24">
             <h1 className="text-[32px]">타입스크립트 연습 {user.name}</h1>
+            <p>문의 : {user.email}</p>
         </main>
     );
 }
